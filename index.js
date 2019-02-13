399123ad5c244125fc7772b7ba23b8b0bec97a27
Robin Drexler <drexler.robin@gmail.com>
2019-02-13 19:22:59 +0100

    feat: support import statements

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ function getFilePathArgument(path, t, state) {
   );
 }
 
+/**
+ *
+ */
+function handleFactoryFunction(variableName, path, t, state) {
+  const paths = path.scope.getBinding(variableName).referencePaths;
+  if (!paths.length) {
+    return;
+  }
+
+  // We only care about function invocations
+  // Statements like `if (debug)` can be left untouched
+  const variables = paths.filter(path => path.findParent(t.isCallExpression));
+  variables.forEach(path => {
+    // `debug('foo')('bar')`
+    const epxressionStatement = path.findParent(t.isExpressionStatement);
+    if (epxressionStatement) {
+      return epxressionStatement
+        .get('expression')
+        .node.arguments.push(getFilePathArgument(path, t, state));
+    }
+
+    // const log = debug('foo');
+    // log('bar')
+    return handleIdentifier(path.findParent(t.isVariableDeclarator), t, state);
+  });
+}
+
 function handleIdentifier(path, t, state) {
   const variableName = path.node.id.name;
   const paths = path.scope.getBinding(variableName).referencePaths;
@@ -26,17 +53,28 @@ module.exports = function(babel) {
   const { types: t } = babel;
   return {
     visitor: {
+      ImportDefaultSpecifier(path, state) {
+        if (path.parentPath.get('source').node.value !== 'debug') {
+          return;
+        }
+        return handleFactoryFunction(
+          path.get('local').node.name,
+          path,
+          t,
+          state
+        );
+      },
       CallExpression(path, state) {
-        if (path.node.callee.name !== "require") {
+        if (path.node.callee.name !== 'require') {
           return;
         }
 
-        if (path.node.arguments[0].value !== "debug") {
+        if (path.node.arguments[0].value !== 'debug') {
           return;
         }
 
         // immediate invocation `require('debug')('something')`
-        if (t.isCallExpression(path.parentPath.get("callee"))) {
+        if (t.isCallExpression(path.parentPath.get('callee'))) {
           const identifier = path.findParent(t.isVariableDeclarator);
 
           if (!identifier) {
@@ -53,35 +91,16 @@ module.exports = function(babel) {
         // or debug('foo')('bar')
 
         // get whatever var was used to require `debug` to
-        const variableName = path.findParent(t.isVariableDeclarator).node.id
-          .name;
-        const paths = path.scope.getBinding(variableName).referencePaths;
-        if (!paths.length) {
+        const variableDeclaration = path.findParent(t.isVariableDeclarator);
+
+        // `require('debug')` without any variable assignment
+        if (!variableDeclaration) {
           return;
         }
+        const variableName = path.findParent(t.isVariableDeclarator).node.id
+          .name;
 
-        // We only care about function invocations
-        // Statements like `if (debug)` can be left untouched
-        const variables = paths.filter(path =>
-          path.findParent(t.isCallExpression)
-        );
-        variables.forEach(path => {
-          // `debug('foo')('bar')`
-          const epxressionStatement = path.findParent(t.isExpressionStatement);
-          if (epxressionStatement) {
-            return epxressionStatement
-              .get("expression")
-              .node.arguments.push(getFilePathArgument(path, t, state));
-          }
-
-          // const log = debug('foo');
-          // log('bar')
-          return handleIdentifier(
-            path.findParent(t.isVariableDeclarator),
-            t,
-            state
-          );
-        });
+        return handleFactoryFunction(variableName, path, t, state);
       }
     }
   };
